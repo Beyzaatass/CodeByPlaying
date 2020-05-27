9c04b0c1fd50287e1c584aba1980eb168033ddd0
oguzbolme <56116374+oguzbolme@users.noreply.github.com>
2020-05-27 15:17:02 +0300

    Update MutluEt.java

--- ProjeLogin/app/src/main/java/com/example/myapplication/MutluEt.java
+++ ProjeLogin/app/src/main/java/com/example/myapplication/MutluEt.java
@@ -55,7 +55,7 @@ public class MutluEt extends AppCompatActivity {
         });
     }
         private class getResult extends AsyncTask<Void, Void, String> {
-            String host="192.168.1.113";
+            String host=getResources().getString(R.string.ipv4);
             String port="5000";
             String number;
 
